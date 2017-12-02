36cddcfac3fc75734bd6ee0ae7bf7e3fe2af6a2a
Liang Zhao <zhaoliangbz@gmail.com>
2017-12-02 13:41:29 -0800

    init

--- ZenithWebSite/Controllers/HomeController.cs
+++ ZenithWebSite/Controllers/HomeController.cs
@@ -5,28 +5,50 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ZenithWebSite.Models;
+using ZenithWebSite.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace ZenithWebSite.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
-        }
+            DateTime mondayDate = DateTime
+                          .Today
+                          .AddDays(((int)(DateTime.Today.DayOfWeek) * -1) + 1);
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
+            DateTime sundayDate = DateTime
+                          .Today
+                          .AddDays(((int)(DateTime.Today.DayOfWeek) * -1) + 8);
 
-            return View();
-        }
+            var events = _context.Events.Include(e => e.Activity).ToList();
+            events = events.OrderBy(e => e.EventFrom)
+                .Where(e => e.EventFrom >= mondayDate && e.EventFrom <= sundayDate)
+                .ToList();
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
+            var eventsDetail = new Dictionary<string, List<Event>>();
 
-            return View();
+            foreach (var i in events)
+            {
+                if (eventsDetail.ContainsKey(i.EventFrom.ToLongDateString()))
+                {
+                    eventsDetail[i.EventFrom.ToLongDateString()].Add(i);
+                }
+                else
+                {
+                    eventsDetail[i.EventFrom.ToLongDateString()] = new List<Event> { i };
+                }
+            }
+
+            return View(eventsDetail);
         }
 
         public IActionResult Error()
