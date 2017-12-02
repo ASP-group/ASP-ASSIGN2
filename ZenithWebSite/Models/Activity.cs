341d75b7e25b27647fc583c680f217f8cf65b615
Liang Zhao <zhaoliangbz@gmail.com>
2017-12-02 13:15:56 -0800

    init

---
+++ ZenithWebSite/Models/Activity.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZenithWebSite.Models
+{
+    public class Activity
+    {
+        public int ActivityId { get; set; }
+
+        [Display(Name = "Activity Description"), Required]
+        public string ActivityDescription { get; set; }
+
+        [ScaffoldColumn(false)]
+        public DateTime CreationDate { get; set; }
+
+        public List<Event> Events { get; set; }
+    }
+}
---
+++ ZenithWebSite/Models/Event.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZenithWebSite.Models
+{
+    public class Event
+    {
+
+        public int EventId { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Event Start Time"), Required]
+        public DateTime EventFrom { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Event End Time"), Required]
+        public DateTime EventTo { get; set; }
+
+        [Display(Name = "Created by")]
+        [ScaffoldColumn(false)]
+        public string Username { get; set; }
+
+        [Display(Name = "Activity"), Required]
+        public int ActivityId { get; set; }
+
+        [Display(Name = "Activity")]
+        public virtual Activity Activity { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Created on"), Required]
+        public DateTime CreationDate { get; set; }
+
+        [Display(Name = "Is Active"), Required]
+        public bool IsActive { get; set; }
+    }
+}
